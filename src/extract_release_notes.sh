336cd7e11c249e29b16088d874a3a729ed5755bb
Felix Furrer <ffurrer@gmx.de>
2020-03-16 20:30:42 +0100

    Fix shellcheck warnings

--- src/extract_release_notes.sh
+++ src/extract_release_notes.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 
 extract_release_notes() {
+  # shellcheck disable=SC2154
   readonly changelog_file="${INPUT_CHANGELOG_FILE}"
-  echo "::debug file=.github/actions/extract-release-notes/extract_release_notes.sh,line=6::using input variable: changelog_file=${changelog_file}"
+  echo "::debug file=src/extract_release_notes.sh,line=7::using input variable: changelog_file=${changelog_file}"
 
   release_notes="$(awk '/^## \[[0-9]/ { flag=!flag; count+=1; next } flag && count < 2 {print}' "${changelog_file}" |
     sed '/./,$!d' |
@@ -12,9 +13,9 @@ extract_release_notes() {
 
   if [[ -v INPUT_RELEASE_NOTES_FILE ]] && [[ -n ${INPUT_RELEASE_NOTES_FILE} ]]; then
     readonly release_notes_file="${INPUT_RELEASE_NOTES_FILE}"
-    echo "::debug file=.github/actions/extract-release-notes/extract_release_notes.sh,line=14::using input variable: release_notes_file=${release_notes_file}"
+    echo "::debug file=src/extract_release_notes.sh,line=15::using input variable: release_notes_file=${release_notes_file}"
     echo -n "${release_notes}" >"${release_notes_file}"
-    echo "::debug file=.github/actions/extract-release-notes/extract_release_notes.sh,line=16::writing release notes file: ${release_notes_file}"
+    echo "::debug file=src/extract_release_notes.sh,line=17::writing release notes file: ${release_notes_file}"
   fi
 
   release_notes="${release_notes//'%'/'%25'}"
