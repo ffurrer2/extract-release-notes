8ede7807090495d2dab1523e66d91ae5114c60f4
Ronny Lorenz <ronny@tbi.univie.ac.at>
2023-05-29 16:06:15 +0200

    Add options to specify header level and version prefix

--- src/index.js
+++ src/index.js
@@ -20,7 +20,13 @@ async function main() {
     const prerelease = core.getInput('prerelease')
     core.debug(`prerelease = '${prerelease}'`)
 
-    const releaseNotes = await extractReleaseNotes(changelogFile, prerelease)
+    const versionPrefix = core.getInput('version_prefix')
+    core.debug(`version-prefix = '${versionPrefix}'`)
+
+    const headerLevel = parseInt(core.getInput('header_level'))
+    core.debug(`header_level = '${headerLevel}'`)
+
+    const releaseNotes = await extractReleaseNotes(changelogFile, prerelease, versionPrefix, headerLevel)
     core.debug(`release-notes = '${releaseNotes}'`)
 
     writeReleaseNotesFile(releaseNotesFile, releaseNotes)
@@ -28,18 +34,27 @@ async function main() {
     core.setOutput("release_notes", releaseNotes)
 }
 
-async function extractReleaseNotes(changelogFile, prerelease) {
+async function extractReleaseNotes(changelogFile, prerelease, versionPrefix, headerLevel) {
     const fileStream = fs.createReadStream(changelogFile, {encoding: encoding})
     const rl = readline.createInterface({
         input: fileStream
     })
     const lines = []
     let inside_release = false
+    const header_level = (Number.isInteger(headerLevel) && (headerLevel > 0) && (headerLevel < 7)) ? headerLevel : 2
+    const level_match_regex = "^#{" + header_level + "}\\s+"
+    const levelup_match_regex = (header_level > 1) ? "^#{1," + (header_level - 1) + "}\\s+" : level_match_regex
+    const version_match_regex = "\\[" + versionPrefix + "\\s*[0-9]"
+    const unreleases_match_regex = "\\[Unreleased\\]"
+    core.debug(`version_match_regex: '${version_match_regex}'`)
+    core.debug(`unrelease_match_regex: '${unreleases_match_regex}'`)
     for await (const line of rl) {
-        const start_of_release = (!!line.match("^#+ \\[[0-9]") || (prerelease === 'true' && !!line.match("^#+ \\[Unreleased\\]")))
+        let header_match = (!!line.match(level_match_regex))
+        let headerup_match = (!!line.match(levelup_match_regex))
+        let start_of_release = (!!line.match(level_match_regex + version_match_regex) || (prerelease === 'true' && !!line.match(level_match_regex + unreleases_match_regex)))
         if (inside_release) {
-            if (start_of_release) {
-                core.debug(`next version found: '${line}'`)
+            if (header_match || headerup_match) {
+                core.debug(`end of version block: '${line}'`)
                 break
             } else {
                 lines.push(line)
